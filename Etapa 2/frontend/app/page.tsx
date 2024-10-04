245f1d382a8386414faa92aca9886c4c886befbd
pukinidev <v.caro@uniandes.edu.co>
2024-10-04 05:55:02 -0500

    pagina

---
+++ Etapa 2/frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "./components/pagecomponents/Home/Hero";
+import ProjectDetail from "./components/pagecomponents/Home/ProjectDetail";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <Hero />
+        <ProjectDetail />
+      </main>
+    </div>
+  );
+}
